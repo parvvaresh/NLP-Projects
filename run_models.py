9039c4fd864cfa60815387523bd5dfcb6fd77f41
reza <parvvaresh@gmail.com>
2024-10-19 00:42:24 +0330

    fix name

--- run_models.py
+++ run_models.py
@@ -1,12 +1,12 @@
-from models.train_models import train_models
+from models.train_models import train_MLmodels
 import pandas as pd
 from PreProcess import PreProcess
 
 def main():
-    df = pd.read_csv("./data/Snappfood.csv").head(10000)
+    df = pd.read_csv("./data/Snappfood.csv")
     result = train_models(df, PreProcess)
-    result.to_csv("./assets/result.csv")
-    print(f"result saved in <<./assets/result.csv>>")
+    result.to_csv("./assets/result_ml.csv")
+    print(f"result saved in <<./assets/result_ml.csv>>")
     
 
 main()
