87ada48169ce24ee4a21c65efa4b9ac11b3b1bd7
reza <parvvaresh@gmail.com>
2024-10-18 17:20:15 +0330

    fix bugs

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 from flask import Flask, render_template, request, make_response
 from pipeline_predict import pipeline_predict
+
 app = Flask(__name__)
-
-
-
-
-
-
 
 @app.route('/')
 def index():
-    response = make_response(render_template('index.html', prediction_text=''))
+    # Render the main page with empty prediction and user input
+    response = make_response(render_template('index.html', prediction_text='', user_input=''))
     response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
     response.headers['Pragma'] = 'no-cache'
     response.headers['Expires'] = '-1'
@@ -19,11 +15,13 @@ def index():
 @app.route('/predict', methods=['POST'])
 def predict():
     user_input = request.form['user_input_text']
+    # Call the prediction pipeline
     prediction = pipeline_predict(user_input).lower()
 
-    
+    # Update the prediction text based on the prediction result
     prediction_text = 'این نظر مثبته :)' if prediction == 'happy' else 'این نظر منفیه :('
     
+    # Render the template with the prediction and user input
     return render_template('index.html', prediction_text=prediction_text, user_input=user_input)
 
 if __name__ == '__main__':
